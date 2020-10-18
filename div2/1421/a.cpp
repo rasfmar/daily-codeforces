49352022eb37527b89c1f810ab4fcbfcb734a6fb
rasfmar <38634617+rasfmar@users.noreply.github.com>
2020-10-18 10:23:47 -0400

    10/18/20 1421A

---
+++ div2/1421/a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+
+        cout << (a ^ b) << '\n';
+    }
+
+    return 0;
+}
